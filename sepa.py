700cfab65717f2912177c800f8021cdeb9258401
wige57 <info@wige-warendorf.de>
2024-09-20 12:11:55 +0200

    New file

---
+++ sepa.py
@@ -0,0 +1,98 @@
+import streamlit as st
+from sepaxml import SepaDD
+import pandas as pd
+import datetime
+import xml.dom.minidom
+
+
+# Header of Streamlit-App
+st.title("SEPA XML Generator")
+
+
+# Excel-File Upload
+uploaded_file = st.file_uploader("Lade deine Excel-Datei hoch", type = "xlsx")
+
+
+if uploaded_file is not None:
+
+    # Import Excel_file
+    #excel_file = "nutzungsentgelte.xlsx"
+    
+    df_config = pd.read_excel(uploaded_file, sheet_name= "config")
+    df_payments = pd.read_excel(uploaded_file, sheet_name= "payments")
+
+
+    # Create config_file
+    config = {
+        "name": df_config.loc[0, 'name'],
+        "IBAN": df_config.loc[0, 'IBAN'].replace(" ", ""),
+        "BIC": df_config.loc[0, 'BIC'],
+        "batch": df_config.loc[0, 'batch'],         # True
+        "creditor_id": df_config.loc[0, 'creditor_id'],
+        "currency": df_config.loc[0, 'currency'],   # ISO 4217
+    }
+
+
+    # Create SEPA-transfer-object
+    sepa = SepaDD(config, clean=True)
+
+
+    # Add single payments -> loop over df_payments
+    for index, row in df_payments.iterrows():
+        payment = {
+            "name": f"{row['Vorname']} {row['Name']}",
+            "IBAN": row['IBAN'].replace(" ", ""),
+            #"BIC": row['BIC'] if pd.notnull(row['BIC']) else "Unknown BIC",  # Fallback falls BIC fehlt
+            "amount": int(row['amount'] * 100),  # Betrag in Euro in Cent umrechnen
+            "type": "RCUR",  # FRST, RCUR, OOFF, FNAL (wiederkehrende Lastschrift)
+            "collection_date": datetime.date.today(),
+            "mandate_id": row['mandate_id'],
+            "mandate_date": pd.to_datetime(row['mandate_date']).date(),
+            "description": row['description'],
+        }
+        # Zahlung zum SEPA-Transfer hinzufügen
+        sepa.add_payment(payment)
+
+# test_iban = "NL50 BANK 1234 5678 90"
+
+# # Zahlung hinzufügen
+# payment = {
+#     "name": "Test von Testenstein",
+#     "IBAN": test_iban.replace(" ", ""),
+#     "BIC": "BANKNL2A",
+#     "amount": 5000,  # in cents
+#     "type" : "RCUR",
+#     "collection_date" : datetime.date.today(),
+#     "mandate_id" : "text29d",
+#     "mandate_date" : datetime.date.today() - datetime.timedelta(days=100),
+#     "description": "Test transaction",
+#     #"execution_date": datetime.date.today() + datetime.timedelta(days=2),
+# }
+
+#sepa.add_payment(payment)
+
+    # Export SEPA-XML
+    xml_content = sepa.export(validate = True)
+
+    # Formatting XML-file
+    dom = xml.dom.minidom.parseString(xml_content)
+    pretty_xml_as_string = dom.toprettyxml()
+
+    # Option for downloading XML-file
+    st.download_button(
+        label = "SEPA-XML-Datei herunterladen",
+        data = xml_content,
+        file_name= "sepa_transfer_.xml",
+        mime="application/xml"
+    )
+
+    # Option zum Anzeigen der XML-Datei
+    st.subheader("SEPA XML Vorschau")
+    st.code(pretty_xml_as_string)
+
+
+# # Save XML-file
+# with open("sepa_transfer_mieten.xml", "wb") as xml_file:
+#     xml_file.write(xml_content)
+
+# print("SEPA-XML-Datei erfolgreich erstellt und gespeichert!")
